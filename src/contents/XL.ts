6124ceab92a6c3fe9c637126ffcb248bd57bf3a1
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-06-28 22:23:47 +0300

    Update XL.ts

--- src/contents/XL.ts
+++ src/contents/XL.ts
@@ -79,6 +79,7 @@ class XL {
       .ele('Ctry', this.xl.GrpHdr.FwdgAgt.BrnchId.PstlAdr.Ctry).up()
       .up()
 
+
       // Payment info part of the message
       .ele('PmtInf')
       .ele('PmtInfId', '').up()
@@ -95,7 +96,6 @@ class XL {
       .up()
       .ele('ReqdExctnDt', '').up()
       .ele('PoolgAdjstmntDt', '').up()
-
 
       .ele('Dbtr')
       .ele('Nm', '').up()
@@ -169,8 +169,7 @@ class XL {
       .ele('Ccy', '').up()
       .ele('Nm', '').up()
       .up()
-
-
+      
       .ele('UltmtDbtr')
       .ele('Nm', '').up()
       .ele('PstlAdr')
@@ -340,275 +339,130 @@ class XL {
       .ele('PstlAdr')
 
       .ele('AdrTp', '').up()
-  .ele('AdrLine', '').up()
-  .ele('StrtNm', '').up()
-  .ele('BldgNb', '').up()
-  .ele('PstCd', '').up()
-  .ele('TwnNm', '').up()
-  .ele('CtrySubDvsn', '').up()
-  .ele('Ctry', '').up()
-    .up()
-    .up()
-    .up()
-        .ele('IntrmyAgt1Acct')
-        .ele('Id')
-        .ele('IBAN', '').up()
-    .up()
-        .ele('Tp')
-        .ele('Cd', '').up()
-    .up()
-  .ele('Ccy', '').up()
-  .ele('Nm', '').up()
-    .up()
-  .ele('IntrmyAgt2)
-  .ele('FinInstnId)
-    .ele('BIC', '').up()
-    .up()
-  .ele('BrnchId)
-  .ele('Id', '').up()
-  .ele('Nm', '').up()
+      .ele('AdrLine', '').up()
+      .ele('StrtNm', '').up()
+      .ele('BldgNb', '').up()
+      .ele('PstCd', '').up()
+      .ele('TwnNm', '').up()
+      .ele('CtrySubDvsn', '').up()
+      .ele('Ctry', '').up()
+      .up()
+      .up()
+      .up()
+
+      .ele('IntrmyAgt1Acct')
+      .ele('Id')
+      .ele('IBAN', '').up()
+      .up()
+      .ele('Tp')
+      .ele('Cd', '').up()
+      .up()
+      .ele('Ccy', '').up()
+      .ele('Nm', '').up()
+      .up()
+      .ele('IntrmyAgt2')
+      .ele('FinInstnId')
+      .ele('BIC', '').up()
+      .up()
+      .ele('BrnchId')
+      .ele('Id', '').up()
+      .ele('Nm', '').up()
       .ele('PstlAdr')
-.ele('AdrTp', '').up()
-  .ele('AdrLine', '').up()
-  .ele('StrtNm', '').up()
-  .ele('BldgNb', '').up()
-  .ele('PstCd', '').up()
-  .ele('TwnNm', '').up()
-  .ele('CtrySubDvsn', '').up()
-  .ele('Ctry', '').up()
-    .up()
-    .up()
-    .up()
-  <IntrmyAgt2Acct>
-  <Id>
-    <IBAN>str1234</IBAN>
-    .up()
-  <Tp>
-  <Cd>CASH</Cd>
-    .up()
-  <Ccy>str1234</Ccy>
-  <Nm>str1234</Nm>
-    .up()
-  <IntrmyAgt3>
-  <FinInstnId>
-    <BIC>str1234</BIC>
-    .up()
-  <BrnchId>
-  <Id>str1234</Id>
-  <Nm>str1234</Nm>
-  <PstlAdr>
-.ele('AdrTp', '').up()
-  .ele('AdrLine', '').up()
-  .ele('StrtNm', '').up()
-  .ele('BldgNb', '').up()
-  .ele('PstCd', '').up()
-  .ele('TwnNm', '').up()
-  .ele('CtrySubDvsn', '').up()
-  .ele('Ctry', '').up()
-    .up()
-    .up()
-    .up()
-  <IntrmyAgt3Acct>
-  <Id>
-    <IBAN>str1234</IBAN>
-    .up()
-  <Tp>
-  <Cd>CASH</Cd>
-    .up()
-  <Ccy>str1234</Ccy>
-  <Nm>str1234</Nm>
-    .up()
-  <CdtrAgt>
-  <FinInstnId>
-    <BIC>str1234</BIC>
-    .up()
-  <BrnchId>
-  <Id>str1234</Id>
-  <Nm>str1234</Nm>
-  <PstlAdr>
-.ele('AdrTp', '').up()
-  .ele('AdrLine', '').up()
-  .ele('StrtNm', '').up()
-  .ele('BldgNb', '').up()
-  .ele('PstCd', '').up()
-  .ele('TwnNm', '').up()
-  .ele('CtrySubDvsn', '').up()
-  .ele('Ctry', '').up()
-    .up()
-    .up()
-    .up()
-  <CdtrAgtAcct>
-  <Id>
-    <IBAN>str1234</IBAN>
-    .up()
-  <Tp>
-  <Cd>CASH</Cd>
-    .up()
-  <Ccy>str1234</Ccy>
-  <Nm>str1234</Nm>
-    .up()
-  <Cdtr>
-  <Nm>str1234</Nm>
-  <PstlAdr>
-.ele('AdrTp', '').up()
-  .ele('AdrLine', '').up()
-  .ele('StrtNm', '').up()
-  .ele('BldgNb', '').up()
-  .ele('PstCd', '').up()
-  .ele('TwnNm', '').up()
-  .ele('CtrySubDvsn', '').up()
-  .ele('Ctry', '').up()
-    .up()
+      .ele('AdrTp', '').up()
+      .ele('AdrLine', '').up()
+      .ele('StrtNm', '').up()
+      .ele('BldgNb', '').up()
+      .ele('PstCd', '').up()
+      .ele('TwnNm', '').up()
+      .ele('CtrySubDvsn', '').up()
+      .ele('Ctry', '').up()
+      .up()
+      .up()
+      .up()
+
+      .ele('IntrmyAgt2Acct')
+      .ele('Id')
+      .ele('IBAN', '').up()
+      .up()
+      .ele('Tp')
+      .ele('Cd', '').up()
+      .up()
+      .ele('Ccy', '').up()
+      .ele('Nm', '').up()
+      .up()
+      .ele('IntrmyAgt3')
+      .ele('FinInstnId')
+      .ele('BIC', '').up()
+      .up()
+      .ele('BrnchId')
+      .ele('Id', '').up()
+      .ele('Nm', '').up()
+      .ele('PstlAdr')
+      .ele('AdrTp', '').up()
+      .ele('AdrLine', '').up()
+      .ele('StrtNm', '').up()
+      .ele('BldgNb', '').up()
+      .ele('PstCd', '').up()
+      .ele('TwnNm', '').up()
+      .ele('CtrySubDvsn', '').up()
+      .ele('Ctry', '').up()
+      .up()
+      .up()
+      .up()
+      .ele('IntrmyAgt3Acct')
+      .ele('Id')
+      .ele('IBAN', '').up()
+      .up()
+      .ele('Tp')
+      .ele('Cd', '').up()
+      .up()
+      .ele('Ccy', '').up()
+      .ele('Nm', '').up()
+      .up()
+      .ele('CdtrAgt')
+      .ele('FinInstnId')
+      .ele('BIC', '').up()
+      .up()
+      .ele('BrnchId')
+      .ele('Id', '').up()
+      .ele('Nm', '').up()
+      .ele('PstlAdr')
+      .ele('AdrTp', '').up()
+      .ele('AdrLine', '').up()
+      .ele('StrtNm', '').up()
+      .ele('BldgNb', '').up()
+      .ele('PstCd', '').up()
+      .ele('TwnNm', '').up()
+      .ele('CtrySubDvsn', '').up()
+      .ele('Ctry', '').up()
+      .up()
+      .up()
+      .up()
+      .ele('CdtrAgtAcct')
+      .ele('Id')
+      .ele('IBAN', '').up()
+      .up()
+      .ele('Tp')
+      .ele('Cd', '').up()
+      .up()
+      .ele('Ccy', '').up()
+      .ele('Nm', '').up()
+      .up()
+      .ele('Cdtr')
+      .ele('Nm', '').up()
+      .ele('PstlAdr')
+      .ele('AdrTp', '').up()
+      .ele('AdrLine', '').up()
+      .ele('StrtNm', '').up()
+      .ele('BldgNb', '').up()
+      .ele('PstCd', '').up()
+      .ele('TwnNm', '').up()
+      .ele('CtrySubDvsn', '').up()
+      .ele('Ctry', '').up()
+      .up()
       .ele('Id')
       .ele('OrgId')
-    .ele('BIC', '').up()
-  .ele('IBEI', '').up()
-  .ele('BEI', '').up()
-  .ele('EANGLN', '').up()
-  .ele('USCHU', '').up()
-  .ele('DUNS', '').up()
-  .ele('BkPtyId', '').up()
-  .ele('TaxIdNb', '').up()
-  .ele('PrtryId').up()
-  .ele('Id', '').up()
-  .ele('Issr', '').up()
-    .up()
-    .up()
-    .up()
-      .ele('CtryOfRes', '').up()
-    .up()
-  <CdtrAcct>
-  <Id>
-    <IBAN>str1234</IBAN>
-    .up()
-  <Tp>
-  <Cd>CASH</Cd>
-    .up()
-  <Ccy>str1234</Ccy>
-  <Nm>str1234</Nm>
-    .up()
-  <UltmtCdtr>
-  <Nm>str1234</Nm>
-  <PstlAdr>
-.ele('AdrTp', '').up()
-  .ele('AdrLine', '').up()
-  .ele('StrtNm', '').up()
-  .ele('BldgNb', '').up()
-  .ele('PstCd', '').up()
-  .ele('TwnNm', '').up()
-  .ele('CtrySubDvsn', '').up()
-  .ele('Ctry', '').up()
-    .up()
-  .ele('Id')
-  .ele('OrgId')
-    .ele('BIC', '').up()
-  .ele('IBEI', '').up()
-  .ele('BEI', '').up()
-  .ele('EANGLN', '').up()
-  .ele('USCHU', '').up()
-  .ele('DUNS', '').up()
-  .ele('BkPtyId', '').up()
-  .ele('TaxIdNb', '').up()
-  .ele('PrtryId').up()
-  .ele('Id', str1234</Id>
-  .ele('Issr', str1234</Issr>
-    .up()
-    .up()
-    .up()
-  <CtryOfRes>str1234</CtryOfRes>
-    .up()
-  <InstrForCdtrAgt>
-  <Cd>CHQB</Cd>
-  <InstrInf>str1234</InstrInf>
-    .up()
-  <InstrForDbtrAgt>str1234</InstrForDbtrAgt>
-  <Purp>
-  <Cd>str1234</Cd>
-    .up()
-  <RgltryRptg>
-  <DbtCdtRptgInd>CRED</DbtCdtRptgInd>
-  <Authrty>
-  <AuthrtyNm>str1234</AuthrtyNm>
-  <AuthrtyCtry>str1234</AuthrtyCtry>
-    .up()
-  <RgltryDtls>
-  <Cd>str</Cd>
-  <Amt Ccy="str1234">123.45</Amt>
-  <Inf>str1234</Inf>
-    .up()
-    .up()
-  <Tax>
-  <CdtrTaxId>str1234</CdtrTaxId>
-  <CdtrTaxTp>str1234</CdtrTaxTp>
-  <DbtrTaxId>str1234</DbtrTaxId>
-  <TaxRefNb>str1234</TaxRefNb>
-  <TtlTaxblBaseAmt Ccy="str1234">123.45</TtlTaxblBaseAmt>
-  <TtlTaxAmt Ccy="str1234">123.45</TtlTaxAmt>
-  <TaxDt>2012-12-13</TaxDt>
-  <TaxTpInf>
-  <CertId>str1234</CertId>
-  <TaxTp>
-  <CtgyDesc>str1234</CtgyDesc>
-  <Rate>123.45</Rate>
-  <TaxblBaseAmt Ccy="str1234">123.45</TaxblBaseAmt>
-  <Amt Ccy="str1234">123.45</Amt>
-  .up()
-  .up()
-  .up()
-  <RltdRmtInf>
-  <RmtId>str1234</RmtId>
-  <RmtLctnMtd>FAXI</RmtLctnMtd>
-  <RmtLctnElctrncAdr>str1234</RmtLctnElctrncAdr>
-  <RmtLctnPstlAdr>
-  <Nm>str1234</Nm>
-  <Adr>
-  <AdrTp>ADDR</AdrTp>
-  <AdrLine>str1234</AdrLine>
-  <StrtNm>str1234</StrtNm>
-  <BldgNb>str1234</BldgNb>
-  <PstCd>str1234</PstCd>
-  <TwnNm>str1234</TwnNm>
-  <CtrySubDvsn>str1234</CtrySubDvsn>
-  <Ctry>str1234</Ctry>
-  .up()
-  .up()
-  .up()
-<RmtInf>
-<Ustrd>str1234</Ustrd>
-<Strd>
-<RfrdDocInf>
-  <RfrdDocTp>
-    <Cd>MSIN</Cd>
-<Issr>str1234</Issr>
-  .up()
-<RfrdDocNb>str1234</RfrdDocNb>
-  .up()
-<RfrdDocRltdDt>2012-12-13</RfrdDocRltdDt>
-<RfrdDocAmt>
-<DuePyblAmt Ccy="str1234">123.45</DuePyblAmt>
-    .up()
-  <CdtrRefInf>
-  <CdtrRefTp>
-    <Cd>RADM</Cd>
-  <Issr>str1234</Issr>
-    .up()
-  <CdtrRef>str1234</CdtrRef>
-    .up()
-  <Invcr>
-  <Nm>str1234</Nm>
-  <PstlAdr>
-.ele('AdrTp', '').up()
-  .ele('AdrLine', '').up()
-  .ele('StrtNm', '').up()
-  .ele('BldgNb', '').up()
-  .ele('PstCd', '').up()
-  .ele('TwnNm', '').up()
-  .ele('CtrySubDvsn', '').up()
-  .ele('Ctry', '').up()
-    .up()
-  <Id>
-  <OrgId>
-    <BIC>str1234</BIC>
+      .ele('BIC', '').up()
       .ele('IBEI', '').up()
       .ele('BEI', '').up()
       .ele('EANGLN', '').up()
@@ -616,28 +470,148 @@ class XL {
       .ele('DUNS', '').up()
       .ele('BkPtyId', '').up()
       .ele('TaxIdNb', '').up()
-  <PrtryId>
-  <Id>str1234</Id>
-  <Issr>str1234</Issr>
-    .up()
-    .up()
-    .up()
-  <CtryOfRes>str1234</CtryOfRes>
-    .up()
-  <Invcee>
-  <Nm>str1234</Nm>
-  <PstlAdr>
-.ele('AdrTp', '').up()
-  .ele('AdrLine', '').up()
-  .ele('StrtNm', '').up()
-  .ele('BldgNb', '').up()
-  .ele('PstCd', '').up()
-  .ele('TwnNm', '').up()
-  .ele('CtrySubDvsn', '').up()
-  .ele('Ctry', '').up()
-    .up()
-  .ele('Id')
-  .ele('OrgId')
+      .ele('PrtryId').up()
+      .ele('Id', '').up()
+      .ele('Issr', '').up()
+      .up()
+      .up()
+      .up()
+      .ele('CtryOfRes', '').up()
+      .up()
+      .ele('CdtrAcct')
+      .ele('Id')
+      .ele('IBAN', '').up()
+      .up()
+      .ele('Tp')
+      .ele('Cd', '').up()
+      .up()
+      .ele('Ccy', '').up()
+      .ele('Nm', '').up()
+      .up()
+      .ele('UltmtCdtr')
+      .ele('Nm', '').up()
+      .ele('PstlAdr')
+      .ele('AdrTp', '').up()
+      .ele('AdrLine', '').up()
+      .ele('StrtNm', '').up()
+      .ele('BldgNb', '').up()
+      .ele('PstCd', '').up()
+      .ele('TwnNm', '').up()
+      .ele('CtrySubDvsn', '').up()
+      .ele('Ctry', '').up()
+      .up()
+      .ele('Id')
+      .ele('OrgId')
+      .ele('BIC', '').up()
+      .ele('IBEI', '').up()
+      .ele('BEI', '').up()
+      .ele('EANGLN', '').up()
+      .ele('USCHU', '').up()
+      .ele('DUNS', '').up()
+      .ele('BkPtyId', '').up()
+      .ele('TaxIdNb', '').up()
+      .ele('PrtryId').up()
+      .ele('Id', '').up()
+      .ele('Issr', '').up()
+
+
+      .up()
+      .up()
+      .up()
+      .ele('CtryOfRes', '').up()
+      .up()
+      .ele('InstrForCdtrAgt')
+      .ele('Cd', '').up()
+      .ele('InstrInf', '').up()
+      .up()
+      .ele('InstrForDbtrAgt', '').up()
+      .ele('Purp')
+      .ele('Cd', '').up()
+      .up()
+      .ele('RgltryRptg')
+      .ele('DbtCdtRptgInd', '').up()
+      .ele('Authrty')
+      .ele('AuthrtyNm', '').up()
+      .ele('AuthrtyCtry', '').up()
+      .up()
+      .ele('RgltryDtls')
+      .ele('Cd', '').up()
+      .ele('Amt', 123.45, {Ccy: 'str1234'}).up()
+      .ele('Inf', '').up()
+      .up()
+      .up()
+      .ele('Tax')
+      .ele('CdtrTaxId', '').up()
+      .ele('CdtrTaxTp', '').up()
+      .ele('DbtrTaxId', '').up()
+      .ele('TaxRefNb', '').up()
+      .ele('TtlTaxblBaseAmt', 123.45, {Ccy: 'str1234'}).up()
+      .ele('TtlTaxAmt', 123.45, {Ccy: 'str1234'}).up()
+      .ele('TaxDt', '').up()
+      .ele('TaxTpInf')
+      .ele('CertId', '').up()
+      .ele('TaxTp')
+      .ele('CtgyDesc', '').up()
+      .ele('Rate', '').up()
+      .ele('TaxblBaseAmt', 123.45, {Ccy: 'str1234'}).up()
+      .ele('Amt', 123.45, {Ccy: 'str1234'}).up()
+      .up()
+      .up()
+      .up()
+
+      .ele('RltdRmtInf')
+      .ele('RmtId', '').up()
+      .ele('RmtLctnMtd', '').up()
+      .ele('RmtLctnElctrncAdr', '').up()
+      .ele('RmtLctnPstlAdr')
+      .ele('Nm', '').up()
+      .ele('Adr')
+      .ele('AdrTp', '').up()
+      .ele('AdrLine', '').up()
+      .ele('StrtNm', '').up()
+      .ele('BldgNb', '').up()
+      .ele('PstCd', '').up()
+      .ele('TwnNm', '').up()
+      .ele('CtrySubDvsn', '').up()
+      .ele('Ctry', '').up()
+      .up()
+      .up()
+      .up()
+      .ele('RmtInf')
+      .ele('Ustrd', '').up()
+      .ele('Strd')
+      .ele('RfrdDocInf')
+      .ele('RfrdDocTp')
+      .ele('Cd', '').up()
+      .ele('Issr', '').up()
+      .up()
+      .ele('RfrdDocNb', '').up()
+      .up()
+      .ele('RfrdDocRltdDt', '').up()
+      .ele('RfrdDocAmt')
+      .ele('DuePyblAmt', 123.45, {Ccy: 'str1234'}).up()
+      .up()
+      .ele('CdtrRefInf')
+      .ele('CdtrRefTp')
+      .ele('Cd', '').up()
+      .ele('Issr', '').up()
+      .up()
+      .ele('CdtrRef', '').up()
+      .up()
+      .ele('Invcr')
+      .ele('Nm', '').up()
+      .ele('PstlAdr')
+      .ele('AdrTp', '').up()
+      .ele('AdrLine', '').up()
+      .ele('StrtNm', '').up()
+      .ele('BldgNb', '').up()
+      .ele('PstCd', '').up()
+      .ele('TwnNm', '').up()
+      .ele('CtrySubDvsn', '').up()
+      .ele('Ctry', '').up()
+      .up()
+      .ele('Id')
+      .ele('OrgId')
       .ele('BIC', '').up()
       .ele('IBEI', '').up()
       .ele('BEI', '').up()
@@ -647,19 +621,47 @@ class XL {
       .ele('BkPtyId', '').up()
       .ele('TaxIdNb', '').up()
       .ele('PrtryId')
-  .ele('Id', '').up())
-  .ele('Issr', '').up())
-    .up()
-    .up()
-    .up()
-  .ele('CtryOfRes', '').up()
-    .up()
-  .ele('AddtlRmtInf', '').up()
-    .up()
-    .up()
-
-   */
-
+      .ele('Id', '').up()
+      .ele('Issr', '').up()
+      .up()
+      .up()
+      .up()
+      .ele('CtryOfRes', '').up()
+      .up()
+      .ele('Invcee')
+      .ele('Nm', '').up()
+      .ele('PstlAdr')
+      .ele('AdrTp', '').up()
+      .ele('AdrLine', '').up()
+      .ele('StrtNm', '').up()
+      .ele('BldgNb', '').up()
+      .ele('PstCd', '').up()
+      .ele('TwnNm', '').up()
+      .ele('CtrySubDvsn', '').up()
+      .ele('Ctry', '').up()
+      .up()
+      .ele('Id')
+      .ele('OrgId')
+      .ele('BIC', '').up()
+      .ele('IBEI', '').up()
+      .ele('BEI', '').up()
+      .ele('EANGLN', '').up()
+      .ele('USCHU', '').up()
+      .ele('DUNS', '').up()
+      .ele('BkPtyId', '').up()
+      .ele('TaxIdNb', '').up()
+      .ele('PrtryId')
+      .ele('Id', '').up()
+      .ele('Issr', '').up()
+      .up()
+      .up()
+      .up()
+      .ele('CtryOfRes', '').up()
+      .up()
+      .ele('AddtlRmtInf', '').up()
+      .up()
+      .up()
+      */
 
     return xml.end({pretty: true});
   }
