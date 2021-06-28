2fa4103dcf3a5db04f9ca192361afe9142fe49be
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-06-28 12:46:49 +0300

    XL message development

--- src/contents/XL.ts
+++ src/contents/XL.ts
@@ -19,7 +19,81 @@ class XL {
   }
 
   createXmlBody(): string {
+    let xml: xmlBuilder.XMLElement = xmlBuilder.create('Document', {version: '1.0', encoding: 'utf-8'})
+      .ele('pain.001.001.02')
+      .ele('GrpHdr') // Group header
 
+      .ele('MsgId', '').up()
+      .ele('CreDtTm', '').up()
+      .ele('Authstn', '').up()
+      .ele('BtchBookg', '').up()
+      .ele('NbOfTxs', '').up()
+      .ele('CtrlSum', '').up()
+      .ele('Grpg', '').up()
+      .ele('InitgPty')
+      .ele('Nm', '').up()
+      .ele('PstlAdr')
+      .ele('AdrTp', '').up()
+      .ele('AdrLine', '').up()
+      .ele('StrtNm', '').up()
+      .ele('BldgNb', '').up()
+      .ele('PstCd', '').up()
+      .ele('TwnNm', '').up()
+      .ele('CtrySubDvsn', '').up()
+      .ele('Ctry', '').up()
+      .up()
+      .ele('Id')
+      .ele('OrgId')
+      .ele('BIC', '').up()
+      .ele('IBEI', '').up()
+      .ele('BEI', '').up()
+      .ele('EANGLN', '').up()
+      .ele('USCHU', '').up()
+      .ele('DUNS', '').up()
+      .ele('BkPtyId', '').up()
+      .ele('TaxIdNb', '').up()
+      .ele('PrtryId')
+      .ele('id', '').up()
+      .ele('Issr', '').up()
+      .up()
+      .up()
+      .ele('CtryOfRes', '').up()
+      .up()
+      .up()
+      .ele('FwdgAgt')
+
+      .ele('FinInstnId')
+      .ele('BIC', '').up()
+      .up()
+      .ele('BrnchId')
+      .ele('Id', '').up()
+      .ele('Nm', '').up()
+      .ele('PstlAdr')
+      .ele('AdrTp', '').up()
+      .ele('AdrLine', '').up()
+      .ele('StrtNm', '').up()
+      .ele('BldgNb', '').up()
+      .ele('PstCd', '').up()
+      .ele('TwnNm', '').up()
+      .ele('CtrySubDvsn', '').up()
+      .ele('Ctry', '').up()
+      .up()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      .ele('PmtInf').up()
+    return xml.end({pretty: true});
   }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,31 @@ import * as path from 'path';
 
 const soap = require('strong-soap').soap;
 import {ApplicationRequest} from './contents/applicationRequest';
+import {XL} from './contents/XL';
 import {Bank, Environment, FileTypes, Operations} from './constants';
 import {UserParamsInterface, SoftwareIdInterface} from './interfaces';
 import {GetWSDL} from './utils';
 
-// const applicationRequest = new ApplicationRequest(
-//   '123456', Operations.downloadFile, '', '', 'NEW',
-//   ['test1', 'test2', 'test3'], 'TestFileName', false, 0, 0,
-//   {name: 'Test', version: '0.9.0'} as SoftwareIdInterface, FileTypes.XT
-// );
-// console.log(applicationRequest.createXmlBody());
-
 
 async function UploadFile(userParams: UserParamsInterface, uploadFileParams: any) {
-  const WSDL = soap.WSDL;
-  const options = {};
-  WSDL.open(GetWSDL(userParams.environment, userParams.bank), options,
-    function (err: any, wsdl: any) {
-      const downloadFileOp = wsdl.definitions.bindings.CorporateFileServiceHttpBinding.operations.downloadFile;
-      console.log(downloadFileOp.$name)
-    });
+  // const WSDL = soap.WSDL;
+  // const options = {};
+  // WSDL.open(GetWSDL(userParams.environment, userParams.bank), options,
+  //   function (err: any, wsdl: any) {
+  //     const downloadFileOp = wsdl.definitions.bindings.CorporateFileServiceHttpBinding.operations.downloadFile;
+  //     console.log(downloadFileOp.$name)
+  //   });
+
+  // const applicationRequest = new ApplicationRequest(
+  //   '123456', Operations.downloadFile, '', '', 'NEW',
+  //   ['test1', 'test2', 'test3'], 'TestFileName', false, 0, 0,
+  //   {name: 'Test', version: '0.9.0'} as SoftwareIdInterface, FileTypes.XT
+  // );
+  // console.log(applicationRequest.createXmlBody());
+
+  const xl = new XL('');
+  console.log(xl.createXmlBody())
+
 }
 
 // @ts-ignore
