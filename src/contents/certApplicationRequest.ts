024cbe9d76681f12084fc747329c4336790443cc
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-06-29 17:07:39 +0300

    Initial cert application request xml generator

--- src/contents/certApplicationRequest.ts
+++ src/contents/certApplicationRequest.ts
@@ -1,22 +1,42 @@
 'use strict';
 
-import * as moment from 'moment'
 import * as xmlBuilder from 'xmlbuilder';
-import {SoftwareIdInterface} from '../interfaces';
-import {Environment, FileType, Operations, Status, StatusValues} from '../constants';
+import {CertApplicationRequestInterface} from '../interfaces';
+import {Environment} from '../constants';
 
 
 class CertApplicationRequest {
 
+  private crp: CertApplicationRequestInterface;
 
-  constructor() {
+  constructor(crp: CertApplicationRequestInterface) {
+    this.crp = crp;
   }
 
-  createXmlBody(): string {
+  public createXmlBody(): string {
     let xml: xmlBuilder.XMLElement = xmlBuilder.create(
-      'ApplicationRequest', {version: '1.0', encoding: 'utf-8'}
+      'CertApplicationRequest', {version: '1.0', encoding: 'utf-8'}
     );
+    xml.ele('CustomerId', this.crp.CustomerId).up()
+      .ele('Timestamp', this.crp.Timestamp).up() // 2012-12-13T12:12:12
+      .ele('Environment', this.crp.Environment).up()
+      .ele('SoftwareId', this.getSoftwareId()).up()
+      .ele('Command', this.crp.Command).up()
+
+      // .ele('Encryption', true).up()
+      // .ele('EncryptionMethod', 'str1234').up()
+      // .ele('Compression', true).up()
+      // .ele('CompressionMethod', 'str1234').up()
+      .ele('Service', this.crp.Service).up()
+      .ele('ExecutionSerial', this.crp.ExecutionSerial).up()
+      .ele('Content', this.crp.Content).up();
+    // .ele('TransferKey', ).up()
+    // .ele('SerialNumber', 'str1234').up();
     return xml.end({pretty: true});
+  }
+
+  private getSoftwareId(): string {
+    return this.crp.SoftwareId.name + '-' + this.crp.SoftwareId.version;
   }
 
 }
