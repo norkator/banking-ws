6f7c42a6077f28e3cb428c3d8a12c0e5c0adcea9
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-06-30 17:53:38 +0300

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 import {Bank, Environment} from './constants';
 import * as path from 'path';
+import {readFileSync} from 'fs';
+
 
 /**
  * Get WSDL based on environment
@@ -13,7 +15,13 @@ function Base64DecodeStr(b64string: string): string {
   return Buffer.from(b64string, 'base64').toString('utf-8');
 }
 
+async function LoadFileFromPath(filePath: string) {
+  const file = readFileSync(filePath, 'utf-8');
+  return Buffer.from(file).toString('utf-8');
+}
+
 export {
   GetWSDL,
   Base64DecodeStr,
+  LoadFileFromPath,
 }
