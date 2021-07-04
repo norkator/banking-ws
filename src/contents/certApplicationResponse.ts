7827193c94bee806fdd2afc41fb96c9696523ccb
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-07-04 11:44:27 +0300

    Testing xml2js to parse test response file contents

--- src/contents/certApplicationResponse.ts
+++ src/contents/certApplicationResponse.ts
@@ -1,15 +1,19 @@
 'use strict';
 
+import {parseString} from 'xml2js';
+
 class CertApplicationResponse {
 
   private readonly response: string;
 
   constructor(response: string) {
     this.response = response;
-    this.parseBody();
   }
 
-  private parseBody(): void {
+  public async parseBody(): Promise<void> {
+    const xml = await this.parseXml(this.response);
+
+    console.log(xml);
   }
 
   public isValid(): boolean {
@@ -18,6 +22,16 @@ class CertApplicationResponse {
 
   public getCertificate(): string | undefined {
     return undefined;
+  }
+
+
+  private async parseXml(xmlString: string) {
+    return await new Promise((resolve, reject) => parseString(xmlString, (err, jsonData) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(jsonData);
+    }));
   }
 
 }
