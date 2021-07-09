1036f8bf3994d111361a60e1ecf729bb69725eaa
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-07-09 11:33:44 +0300

    Modify cert request envelope to be right with schemas

--- src/contents/certRequestEnvelope.ts
+++ src/contents/certRequestEnvelope.ts
@@ -16,23 +16,28 @@ class CertRequestEnvelope {
   }
 
   public createXmlBody(): string {
-    let xml: xmlBuilder.XMLElement = xmlBuilder.create('soapenv:Envelope');
+    const envelopeObject = {
+      'soapenv:Envelope': {
+        '@xmlns:opc': 'http://mlp.op.fi/OPCertificateService',
+        '@xmlns:soapenv': 'http://schemas.xmlsoap.org/soap/envelope/',
+        'seapenv:Header': '',
+        'seapenv:Body': {
+          '@wsu:Id': 'id-3',
+          '@xmlns:wsu': 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd',
+          'opc:getCertificatein': {
+            'opc:RequestHeader': {
+              'opc:SenderId': this.senderId,
+              'opc:RequestId': this.requestId,
+              // @ts-ignore
+              'opc:Timestamp': new moment().format('YYYY-MM-DDThh:mm:ssZ')
+            },
+            'cer:ApplicationRequest': this.applicationRequest
+          },
+        },
+      }
+    };
 
-    xml
-      .ele('seapenv:Header').up()
-      .ele('seapenv:Body')
-      .ele('cer:getCertificatein')
-      .ele('cer:RequestHeader')
-      .ele('cer:SenderId', this.senderId).up()
-      .ele('cer:RequestId', this.requestId).up()
-      // @ts-ignore
-      .ele('cer:Timestamp', new moment().format('YYYY-MM-DDThh:mm:ssZ'),).up()
-      .up()
-      .ele('cer:ApplicationRequest', this.applicationRequest)
-      .up()
-      .up()
-      .up();
-
+    let xml: xmlBuilder.XMLElement = xmlBuilder.create(envelopeObject);
     return xml.end({pretty: true});
   }
 
