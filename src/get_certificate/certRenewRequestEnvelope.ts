b891b8c9a2f96f3c684c8e32d0b06bb861933a69
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-07-29 15:52:24 +0300

    Developing signed envelope class forward

--- src/get_certificate/certRenewRequestEnvelope.ts
+++ src/get_certificate/certRenewRequestEnvelope.ts
@@ -2,17 +2,23 @@
 
 import * as xmlBuilder from 'xmlbuilder'
 import * as moment from 'moment';
-import {Base64DecodeStr} from '../utils';
+import {Base64DecodeStr, GetUuid} from '../utils';
 import {GetCertificateInterface} from '../interfaces';
+import {createHash, createSign} from 'crypto';
+import {node} from "webpack";
 
 class CertRenewRequestEnvelope {
 
   private gc: GetCertificateInterface;
   private readonly applicationRequest: string;
+  private readonly timeStampUuid: string;
+  private readonly bodyUuid: string;
 
   constructor(gc: GetCertificateInterface, applicationRequest: string) {
     this.gc = gc;
     this.applicationRequest = applicationRequest;
+    this.timeStampUuid = GetUuid('TS');
+    this.bodyUuid = GetUuid('id');
   }
 
   public createXmlBody(): string {
@@ -21,8 +27,68 @@ class CertRenewRequestEnvelope {
     }
     const signingKey = Base64DecodeStr(this.gc.Base64EncodedClientPrivateKey);
 
-    // noinspection JSDuplicatedDeclaration,JSDuplicatedDeclaration
-    const envelopeObject = {
+    const timeStampNode = {
+      'wsu:Timestamp': {
+        '@wsu:Id': this.timeStampUuid,
+        'wsu:Created': CertRenewRequestEnvelope.getCreated(),
+        'wsu:Expires': CertRenewRequestEnvelope.getExpires(),
+      }
+    };
+
+    const bodyNode = {
+      'soapenv:Body': {
+        '@xmlns:wsu': 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd',
+        '@wsu:Id': this.bodyUuid,
+        'opc:getCertificatein': {
+          'opc:RequestHeader': {
+            'opc:SenderId': this.gc.userParams.customerId,
+            'opc:RequestId': this.gc.RequestId,
+            'opc:Timestamp': moment().format('YYYY-MM-DDThh:mm:ssZ')
+          },
+          'opc:ApplicationRequest': this.applicationRequest,
+        },
+      },
+    };
+
+    const signedInfoNode = {
+      'ds:SignedInfo': {
+        'ds:CanonicalizationMethod': {
+          '@Algorithm': 'http://www.w3.org/2001/10/xml-exc-c14n#'
+        },
+        'ds:SignatureMethod': {
+          '@Algorithm': 'http://www.w3.org/2000/09/xmldsig#rsa-sha1'
+        },
+
+        'ds:Reference': [
+          {
+            '@URI': '#' + this.timeStampUuid,
+            'ds:Transforms': {
+              'ds:Transform': {
+                '@Algorithm': 'http://www.w3.org/2001/10/xml-exc-c14n#'
+              },
+              'ds:DigestMethod': {
+                '@Algorithm': 'http://www.w3.org/2000/09/xmldsig#sha1'
+              },
+              'ds:DigestValue': this.getDigestValue(timeStampNode.toString()),
+            },
+          },
+          {
+            '@URI': '#' + this.bodyUuid,
+            'ds:Transforms': {
+              'ds:Transform': {
+                '@Algorithm': 'http://www.w3.org/2001/10/xml-exc-c14n#'
+              },
+              'ds:DigestMethod': {
+                '@Algorithm': 'http://www.w3.org/2000/09/xmldsig#sha1'
+              },
+              'ds:DigestValue': this.getDigestValue(bodyNode.toString()),
+            }
+          }
+        ],
+      },
+    };
+
+    let envelopeObject = {
       'soapenv:Envelope': {
         '@xmlns:soapenv': 'http://schemas.xmlsoap.org/soap/envelope/',
         '@xmlns:opc': 'http://mlp.op.fi/OPCertificateService',
@@ -38,47 +104,11 @@ class CertRenewRequestEnvelope {
               '@wsu:Id': this.getWsuId(),
               '#text': this.getBinarySecurityToken(),
             },
-            'wsu:Timestamp': {
-              '@wsu:Id': '',
-              'wsu:Created': CertRenewRequestEnvelope.getCreated(),
-              'wsu:Expires': CertRenewRequestEnvelope.getExpires(),
-            },
+            // 'wsu:Timestamp' node is appended here
             'ds:Signature': {
               '@xmlns:ds': 'http://www.w3.org/2000/09/xmldsig#',
-              'ds:SignedInfo': {
-                'ds:CanonicalizationMethod': {
-                  '@Algorithm': 'http://www.w3.org/2001/10/xml-exc-c14n#'
-                },
-                'ds:SignatureMethod': {
-                  '@Algorithm': 'http://www.w3.org/2000/09/xmldsig#rsa-sha1'
-                },
-                'ds:Reference': {
-                  '@URI': '',
-                  'ds:Transforms': {
-                    'ds:Transform': {
-                      '@Algorithm': 'http://www.w3.org/2001/10/xml-exc-c14n#'
-                    },
-                    'ds:DigestMethod': {
-                      '@Algorithm': 'http://www.w3.org/2000/09/xmldsig#sha1'
-                    },
-                    'ds:DigestValue': this.getDigestValue(),
-                  },
-                },
-                // @ts-ignore
-                'ds:Reference': {
-                  '@URI': '',
-                  'ds:Transforms': {
-                    'ds:Transform': {
-                      '@Algorithm': 'http://www.w3.org/2001/10/xml-exc-c14n#'
-                    },
-                    'ds:DigestMethod': {
-                      '@Algorithm': 'http://www.w3.org/2000/09/xmldsig#sha1'
-                    },
-                    'ds:DigestValue': this.getDigestValue(),
-                  }
-                }
-              },
-              'ds:SignatureValue': this.getSignatureValue(),
+              // 'ds:SignedInfo' node is appended here
+              'ds:SignatureValue': this.getSignatureValue(signingKey, bodyNode.toString()),
               'ds:KeyInfo': {
                 'wsse:SecurityTokenReference': {
                   'wsse:Reference': {
@@ -90,18 +120,13 @@ class CertRenewRequestEnvelope {
             }
           }
         },
-        'soapenv:Body': {
-          'opc:getCertificatein': {
-            'opc:RequestHeader': {
-              'opc:SenderId': this.gc.userParams.customerId,
-              'opc:RequestId': this.gc.RequestId,
-              'opc:Timestamp': moment().format('YYYY-MM-DDThh:mm:ssZ')
-            },
-            'opc:ApplicationRequest': this.applicationRequest,
-          },
-        },
+        // 'soapenv:Body' node is appended here
       }
     };
+
+    envelopeObject["soapenv:Envelope"]["soapenv:Header"]["wsse:Security"]["wsu:Timestamp"] = timeStampNode["wsu:Timestamp"];
+    envelopeObject["soapenv:Envelope"]["soapenv:Body"] = bodyNode["soapenv:Body"];
+    envelopeObject["soapenv:Envelope"]["soapenv:Header"]["wsse:Security"]["ds:Signature"]["ds:SignedInfo"] = signedInfoNode["ds:SignedInfo"];
 
 
     let xml_: xmlBuilder.XMLElement = xmlBuilder.create(envelopeObject);
@@ -129,13 +154,22 @@ class CertRenewRequestEnvelope {
     return moment().add(5, 'minutes').format('YYYY-MM-DDThh:mm:ssZ');
   }
 
-  private getSignatureValue(): string {
-    return '';
+  private getDigestValue(node: string): string {
+    var shaSum = createHash('sha1');
+    shaSum.update(node);
+    return shaSum.digest('base64');
   }
 
-  private getDigestValue(): string {
-    return '';
+  private getSignatureValue(signingKey: string, node: string): string {
+    const sign = createSign('rsa-sha1');
+    sign.update(node);
+    sign.end();
+    const signature = sign.sign(signingKey);
+    const sig = signature.toString('base64');
+    console.log(sig);
+    return sig;
   }
+
 
 }
 
