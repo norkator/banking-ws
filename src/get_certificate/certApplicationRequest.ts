3bb26a04ff77bd48d55244b5154279de069ee633
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-07-29 11:57:26 +0300

    Tweaking to support both cases

--- src/get_certificate/certApplicationRequest.ts
+++ src/get_certificate/certApplicationRequest.ts
@@ -2,7 +2,8 @@
 
 import * as xmlBuilder from 'xmlbuilder';
 import {GetCertificateInterface} from '../interfaces';
-import {Base64EncodeStr, LoadFileFromPath} from '../utils';
+import {Base64DecodeStr, Base64EncodeStr, LoadFileFromPath} from '../utils';
+import {SignedXml} from 'xml-crypto';
 
 
 class CertApplicationRequest {
@@ -14,31 +15,53 @@ class CertApplicationRequest {
   }
 
   public async createXmlBody(): Promise<string | undefined> {
-    try {
-      const csr = await LoadFileFromPath(this.gc.CsrPath, 'utf-8');
-      let certRequestObj = {
-        'CertApplicationRequest': {
-          '@xmlns': 'http://op.fi/mlp/xmldata/',
-          '@xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-          '@xsi:schemaLocation': 'http://op.fi/mlp/xmldata/ file:////csamnt1/K830186$/Datapower/schemas/CertApplicationRequest_20090422.xsd',
-          'CustomerId': this.gc.userParams.customerId,
-          'Timestamp': this.gc.Timestamp, // 2012-12-13T12:12:12
-          'Environment': this.gc.userParams.environment,
-          'SoftwareId': this.getSoftwareId(),
-          'Command': this.gc.Command,
-          'Service': 'ISSUER',
-          'Content': Base64EncodeStr(csr), // Base64 encoded -----BEGIN CERTIFICATE REQUEST----- ...
-        }
-      };
-      if (this.gc.Command === 'GetCertificate') {
-        certRequestObj.CertApplicationRequest['TransferKey'] = this.gc.TransferKey === undefined ? '' : this.gc.TransferKey;
+    const csr = await LoadFileFromPath(this.gc.CsrPath, 'utf-8');
+    let certRequestObj = {
+      'CertApplicationRequest': {
+        '@xmlns': 'http://op.fi/mlp/xmldata/',
+        '@xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+        '@xsi:schemaLocation': 'http://op.fi/mlp/xmldata/ file:////csamnt1/K830186$/Datapower/schemas/CertApplicationRequest_20090422.xsd',
+        'CustomerId': this.gc.userParams.customerId,
+        'Timestamp': this.gc.Timestamp, // 2012-12-13T12:12:12
+        'Environment': this.gc.userParams.environment,
+        'SoftwareId': this.getSoftwareId(),
+        'Command': this.gc.Command,
+        'Service': 'ISSUER',
+        'Content': Base64EncodeStr(csr), // Base64 encoded -----BEGIN CERTIFICATE REQUEST----- ...
       }
+    };
+    if (this.gc.Command === 'GetCertificate') {
+      if (this.gc.TransferKey === undefined) {
+        throw new Error('TransferKey cannot be undefined')
+      }
+      certRequestObj.CertApplicationRequest['TransferKey'] = this.gc.TransferKey;
 
       let xml: xmlBuilder.XMLElement = xmlBuilder.create(certRequestObj);
       return xml.end({pretty: true});
-    } catch (e) {
-      return undefined;
     }
+
+    if (this.gc.Command === 'RenewCertificate') {
+      if (this.gc.Base64EncodedClientPrivateKey === undefined) {
+        throw new Error('Base64EncodedClientPrivateKey cannot be undefined with RenewCertificate command')
+      }
+      const signingKey = Base64DecodeStr(this.gc.Base64EncodedClientPrivateKey);
+
+      let xml_: xmlBuilder.XMLElement = xmlBuilder.create(certRequestObj);
+      const xml = xml_.end({pretty: true});
+
+      const sig = new SignedXml();
+      sig.addReference("//*[local-name(.)='CertApplicationRequest']");
+      sig.signingKey = signingKey;
+      // sig.keyInfoProvider = new MyKeyInfo(signingKey);
+      sig.computeSignature(xml);
+
+      // console.log(sig.getSignedXml());
+      // process.exit(0);
+
+      return sig.getSignedXml();
+    }
+
+    return undefined;
   }
 
   private getSoftwareId(): string {
