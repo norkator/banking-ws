b5bc7fb2c8b113fa497b17039cbdadb85d6b5c3f
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-08-11 21:56:15 +0300

    Clean up XTRequestEnvelope to only handle its specific XT body task

--- src/bank_statement/XTRequestEnvelope.ts
+++ src/bank_statement/XTRequestEnvelope.ts
@@ -1,29 +1,20 @@
 'use strict';
 
-import * as xmlBuilder from 'xmlbuilder'
 import * as moment from 'moment';
-import {Base64DecodeStr, Base64EncodeStr, Canonicalize, CleanUpCertificate, GetUuid, LoadFileFromPath} from '../utils';
+import {Base64DecodeStr, CleanUpCertificate, GetUuid} from '../utils';
 import {XTInterface} from '../interfaces';
-import {createHash, createSign} from 'crypto';
+import {EnvelopeSignature} from "../envelopeSignature";
 
 class XTRequestEnvelope {
 
   private xt: XTInterface;
   private readonly applicationRequest: string;
-  private readonly timeStampUuid: string;
   private readonly bodyUuid: string;
-  private readonly binarySecurityTokenUuid: string;
-
-  private readonly CANONICALIZE_METHOD = 'http://www.w3.org/2001/10/xml-exc-c14n#WithComments';
-  private readonly SIGNATURE_METHOD = 'rsa-sha1';
-  private readonly DIGEST_METHOD = 'sha1';
 
   constructor(xt: XTInterface, applicationRequest: string) {
     this.xt = xt;
     this.applicationRequest = applicationRequest;
-    this.timeStampUuid = GetUuid('Timestamp');
     this.bodyUuid = GetUuid('B');
-    this.binarySecurityTokenUuid = GetUuid('CertId');
   }
 
   public async createXmlBody(): Promise<string> {
@@ -32,18 +23,6 @@ class XTRequestEnvelope {
     }
     const signingKey = Base64DecodeStr(this.xt.Base64EncodedClientPrivateKey);
     const binarySecurityToken = CleanUpCertificate(Base64DecodeStr(this.xt.Base64EncodedBankCsr));
-
-
-    const timeStampNode = {
-      'wsu:Timestamp': {
-        '@wsu:Id': this.timeStampUuid,
-        '@xmlns:wsu': 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd',
-        'wsu:Created': this.getCreated(),
-        'wsu:Expires': this.getExpires(),
-      }
-    };
-    const timeStampNodeXml: string = xmlBuilder.create(timeStampNode, {headless: true}).end({pretty: false});
-    const canonicalizeTimeSampNodeXml = await Canonicalize(timeStampNodeXml, this.CANONICALIZE_METHOD);
 
 
     const bodyNode = {
@@ -68,111 +47,11 @@ class XTRequestEnvelope {
         },
       },
     };
-    let bodyNodeXml: string = xmlBuilder.create(bodyNode, {headless: true}).end({pretty: false});
-    let canonicalizeBodyNodeXml = await Canonicalize(bodyNodeXml, this.CANONICALIZE_METHOD);
 
-    const signedInfoNode = {
-      'ds:SignedInfo': {
-        '@xmlns:ds': 'http://www.w3.org/2000/09/xmldsig#',
-        'ds:CanonicalizationMethod': {
-          '@Algorithm': this.CANONICALIZE_METHOD
-        },
-        'ds:SignatureMethod': {
-          '@Algorithm': 'http://www.w3.org/2000/09/xmldsig#' + this.SIGNATURE_METHOD
-        },
-
-        'ds:Reference': [
-          {
-            '@URI': '#' + this.timeStampUuid,
-            'ds:Transforms': {
-              'ds:Transform': {
-                '@Algorithm': this.CANONICALIZE_METHOD
-              },
-            },
-            'ds:DigestMethod': {
-              '@Algorithm': 'http://www.w3.org/2000/09/xmldsig#' + this.DIGEST_METHOD
-            },
-            'ds:DigestValue': this.getDigestValue(canonicalizeTimeSampNodeXml),
-          },
-          {
-            '@URI': '#' + this.bodyUuid,
-            'ds:Transforms': {
-              'ds:Transform': {
-                '@Algorithm': this.CANONICALIZE_METHOD
-              },
-            },
-            'ds:DigestMethod': {
-              '@Algorithm': 'http://www.w3.org/2000/09/xmldsig#' + this.DIGEST_METHOD
-            },
-            'ds:DigestValue': this.getDigestValue(canonicalizeBodyNodeXml),
-          }
-        ],
-      },
-    };
-    const signedInfoNodeXml: string = xmlBuilder.create(signedInfoNode, {headless: true}).end({pretty: false});
-    const canonicalizeSignedInfoNodeXml = await Canonicalize(signedInfoNodeXml, this.CANONICALIZE_METHOD);
-
-    let envelopeObject = {
-      'soapenv:Envelope': {
-        '@xmlns:cor': 'http://bxd.fi/CorporateFileService',
-        '@xmlns:mod': 'http://model.bxd.fi',
-        '@xmlns:soapenv': 'http://schemas.xmlsoap.org/soap/envelope/',
-        '@xmlns:ds': 'http://www.w3.org/2000/09/xmldsig#',
-        'soapenv:Header': {
-          'wsse:Security': {
-            '@xmlns:wsse': 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd',
-            '@xmlns:wsu': 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd',
-            '@soapenv:mustUnderstand': '1',
-            '#text': [
-              // 'wsu:Timestamp' node is appended here
-              {
-                'wsse:BinarySecurityToken': {
-                  '@EncodingType': 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary',
-                  '@ValueType': 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3',
-                  '@wsu:Id': this.binarySecurityTokenUuid,
-                  '@xmlns:wsu': 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd',
-                  '#text': binarySecurityToken,
-                },
-              },
-              {
-                'ds:Signature': {
-                  '@xmlns:ds': 'http://www.w3.org/2000/09/xmldsig#',
-                  '#text': [
-                    // 'ds:SignedInfo' node is appended here
-                    {
-                      'ds:SignatureValue': this.getSignatureValue(signingKey, canonicalizeSignedInfoNodeXml),
-                    },
-                    {
-                      'ds:KeyInfo': {
-                        'wsse:SecurityTokenReference': {
-                          '@xmlns': '',
-                          'wsse:Reference': {
-                            '@URI': '#' + this.binarySecurityTokenUuid,
-                            '@ValueType': 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3'
-                          }
-                        }
-                      },
-                    }
-                  ],
-                }
-              },
-            ]
-          }
-        },
-        // 'soapenv:Body' node is appended here
-      }
-    };
-
-    // @ts-ignore
-    envelopeObject["soapenv:Envelope"]["soapenv:Header"]["wsse:Security"]["#text"].unshift({'wsu:Timestamp': timeStampNode["wsu:Timestamp"]});
-    // @ts-ignore
-    envelopeObject["soapenv:Envelope"]["soapenv:Body"] = bodyNode["soapenv:Body"];
-    // @ts-ignore
-    envelopeObject["soapenv:Envelope"]["soapenv:Header"]["wsse:Security"]["#text"][2]["ds:Signature"]["#text"].unshift({'ds:SignedInfo': signedInfoNode["ds:SignedInfo"]});
-
-
-    let xml_: xmlBuilder.XMLElement = xmlBuilder.create(envelopeObject);
-    const xml = xml_.end({pretty: false});
+    const envelopeSignature = new EnvelopeSignature();
+    const xml = await envelopeSignature.constructEnvelopeWithSignature(
+      bodyNode, this.bodyUuid, signingKey, binarySecurityToken
+    );
 
     // console.log(xml);
     // process.exit(0);
@@ -180,34 +59,9 @@ class XTRequestEnvelope {
     return xml;
   }
 
-  // noinspection JSMethodCanBeStatic
-  private getCreated(): string {
-    return moment().toISOString();
-  }
-
-  // noinspection JSMethodCanBeStatic
-  private getExpires(): string {
-    return moment().add(5, 'minutes').toISOString();
-  }
 
   private getSoftwareId(): string {
     return this.xt.SoftwareId.name + '-' + this.xt.SoftwareId.version;
-  }
-
-  // noinspection JSMethodCanBeStatic
-  private getDigestValue(node: string): string {
-    const shaSum = createHash(this.DIGEST_METHOD);
-    shaSum.update(node);
-    return shaSum.digest('base64');
-  }
-
-  // noinspection JSMethodCanBeStatic
-  private getSignatureValue(signingKey: string, node: string): string {
-    const sign = createSign(this.SIGNATURE_METHOD);
-    sign.update(node);
-    sign.end();
-    const signature = sign.sign(signingKey);
-    return signature.toString('base64');
   }
 
 
