6aacaf510cb82f42cff775369311ac7b76cf3303
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2022-12-31 00:23:26 +0200

    add test for checking certificate info

--- src/create_certificate/CreateCertificate.test.ts
+++ src/create_certificate/CreateCertificate.test.ts
@@ -1,15 +1,14 @@
 // import * as mocha from 'mocha';
 import * as chai from 'chai';
-import {CreateOwnCertificate} from '../index';
-import {CreatedCertificateInterface} from '../interfaces';
+import {CreateOwnCertificate, CheckOwnCertificate} from '../index';
+import {CreateCertificateInterface, CreatedCertificateInterface} from '../interfaces';
 import {Base64DecodeStr} from '../utils';
 
 const expect = chai.expect;
 describe('CreateCertificate', async () => {
 
   it('should create certificate and contain expected structure', async () => {
-
-    const certificate: CreatedCertificateInterface = await CreateOwnCertificate({
+    const cc: CreateCertificateInterface = {
       twoLetterCountryCode: 'FI',
       stateOrProvince: 'Test province',
       city: 'Test city',
@@ -17,15 +16,24 @@ describe('CreateCertificate', async () => {
       companyUnitName: 'Test company unit',
       customerId: '12345678',
       emailAddress: 'test.case@example.com',
-    });
+    }
+
+    const certificate: CreatedCertificateInterface = await CreateOwnCertificate(cc);
 
     expect(certificate.clientCertificate).to.not.equal(null);
     expect(certificate.clientPrivateKey).to.not.equal(null);
 
+    // check content structure
     const decodedClientCert = Base64DecodeStr(certificate.clientCertificate);
     console.info(decodedClientCert);
     expect(decodedClientCert).contain('-----BEGIN CERTIFICATE REQUEST-----');
     expect(decodedClientCert).contain('-----END CERTIFICATE REQUEST-----');
+
+    // check decoded certificate info
+    const certInfo = await CheckOwnCertificate(cc);
+    console.info(certInfo);
+    expect(certInfo).contain('Subject: C=FI, ST=Test province, L=Test city, O=Test company, OU=Test company unit, CN=12345678/emailAddress=test.case@example.com');
+    expect(certInfo).contain('Signature Algorithm: sha256WithRSAEncryption');
   });
 });
 
