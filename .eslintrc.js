9e5a2d4a5454b595a0a1fc46f785be2bc93722e3
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2023-01-17 23:12:18 +0200

    modify eslint rules, allow use of any and use single quotes around project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,17 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: "module" // Allows for the use of imports
   },
   extends: [
-    "plugin:@typescript-eslint/recommended" // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/recommended' // Uses the recommended rules from the @typescript-eslint/eslint-plugin
   ],
   rules: {
     // "quotes": [2, "single", {"avoidEscape": true}], // not working
     '@typescript-eslint/no-var-requires': 0,
     '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/no-explicit-any': 'off',
+    'quotes': [2, 'single', {'avoidEscape': true}],
   }
 };
