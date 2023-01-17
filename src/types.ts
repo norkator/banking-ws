c6777676c1f15a5b2583cbd108591e296accedc6
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2023-01-17 20:02:32 +0200

    clean types file

--- src/types.ts
+++ src/types.ts
@@ -1,32 +1,30 @@
 export type Environment = 'TEST' | 'PRODUCTION';
 export type Bank = 'Samlink';
-export type WsdlType = 'cert_' | '';
 export type FileType = 'XL' | 'XT';
 export type Status = 'NEW' | 'DLD' | 'ALL' | '';
 export type Currency = 'EUR' | 'USD';
 export type PaymentMethod = 'TRF';
 export type ServiceLevel = 'SLEV';
 export type Command = 'GetCertificate' | 'RenewCertificate';
-export type Service = 'ISSUER' | 'MATU';
 export type OutputEncoding = 'utf-8' | 'base64';
 export type Language = 'FI' | 'EN' | 'SV';
 export type Country = 'FI' | 'SE'; // Finland, Sweden,
 export type Code = 'BANK';
 export type BIC =
-    'HELSFIHH'
-    | 'BIGKFIH1'
-    | 'CITIFIHX'
-    | 'DABAFIHH'
-    | 'DABAFIHX'
-    | 'DNBAFIHX'
-    | 'HANDFIHH'
-    | 'HOLVFIHH'
-    | 'NDEAFIHH'
-    | 'OMASP'
-    | 'OKOYFIHH'
-    | 'POPFFI22'
-    | 'SBANFIHH'
-    | 'ESSEFIHX'
-    | 'SWEDFIHH'
-    | 'ITELFIHH'
-    | 'AABAFI22'
+  'HELSFIHH'
+  | 'BIGKFIH1'
+  | 'CITIFIHX'
+  | 'DABAFIHH'
+  | 'DABAFIHX'
+  | 'DNBAFIHX'
+  | 'HANDFIHH'
+  | 'HOLVFIHH'
+  | 'NDEAFIHH'
+  | 'OMASP'
+  | 'OKOYFIHH'
+  | 'POPFFI22'
+  | 'SBANFIHH'
+  | 'ESSEFIHX'
+  | 'SWEDFIHH'
+  | 'ITELFIHH'
+  | 'AABAFI22';
