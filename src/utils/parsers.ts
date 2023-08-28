86104508089c25d3ee5b641754b89da525221f9f
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2023-08-28 16:42:24 +0300

    parsing development forward for bank statement

--- src/utils/parsers.ts
+++ src/utils/parsers.ts
@@ -1,5 +1,5 @@
 import {Base64DecodeStr, ParseXml, GetNested, GetExternalStatusCodeDescriptions} from './utils';
-import {PaymentStatusReport} from '../interfaces';
+import {BankStatement, PaymentStatusReport} from '../interfaces';
 
 /**
  * Parses content from payment status report
@@ -48,11 +48,47 @@ async function ParseContentFromPaymentStatusReport(contentInBase64: string): Pro
 }
 
 
-async function ParseBankStatement(contentInBase64: string): Promise<any> {
-   const a = await ParseXml(Base64DecodeStr(contentInBase64)); // eslint-disable-line @typescript-eslint/no-unused-vars
-  // todo bank statement parsing here
+async function ParseBankStatement(contentInBase64: string): Promise<BankStatement> {
+   const a = await ParseXml(Base64DecodeStr(contentInBase64));
+   const BkToCstmrStmt = a['Document']['BkToCstmrStmt'][0]; // bank to customer statement
 
-  return null;
+  return {
+    groupHeader: {
+      messageIdentification: BkToCstmrStmt['GrpHdr'][0]['MsgId'][0],
+      creationDateTime: BkToCstmrStmt['GrpHdr'][0]['CreDtTm'][0],
+    },
+    statement: {
+      statementId : BkToCstmrStmt['Stmt'][0]['Id'][0],
+      legalSequenceNumber: BkToCstmrStmt['Stmt'][0]['LglSeqNb'][0],
+      creationDateTime: BkToCstmrStmt['Stmt'][0]['CreDtTm'][0],
+
+      fromToDatetime: {
+        fromDateTime: BkToCstmrStmt['Stmt'][0]['FrToDt'][0]['FrDtTm'][0],
+        toDateTime: BkToCstmrStmt['Stmt'][0]['FrToDt'][0]['ToDtTm'][0],
+      },
+      account: {
+        identification: {
+          IBAN: BkToCstmrStmt['Stmt'][0]['Acct'][0]['Id'][0]['IBAN'][0],
+        },
+        type: BkToCstmrStmt['Stmt'][0]['Acct'][0]['Tp'][0]['Cd'][0],
+        currency: BkToCstmrStmt['Stmt'][0]['Acct'][0]['Ccy'][0],
+        ownerName: BkToCstmrStmt['Stmt'][0]['Acct'][0]['Ownr'][0]['Nm'][0],
+        servicer: {
+          financialInstitutionId: {
+            BIC: BkToCstmrStmt['Stmt'][0]['Acct'][0]['Svcr'][0]['FinInstnId'][0]['BIC'][0],
+          },
+        },
+      },
+      balance: BkToCstmrStmt['Stmt'][0]['Bal'],
+
+      transactionSummary: {
+        totalEntries: BkToCstmrStmt['Stmt'][0]['TxsSummry'][0]['TtlNtries'][0],
+        totalCreditEntries: BkToCstmrStmt['Stmt'][0]['TxsSummry'][0]['TtlCdtNtries'][0],
+        TotalDebitEntries: BkToCstmrStmt['Stmt'][0]['TxsSummry'][0]['TtlDbtNtries'][0],
+      },
+      statementEntry: BkToCstmrStmt['Stmt'][0]['Ntry'],
+    }
+  };
 }
 
 
